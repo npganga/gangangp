2ab138c927af119822095ed6638f986ccc07dc00
npganga <gangangpalayam@gmail.com>
2017-07-06 14:37:03 +0530

    Update set14.java

--- set14.java
+++ set14.java
@@ -7,11 +7,14 @@ public static void main(String[] args){
  Scanner in=new Scanner(System.in);
  int a=in.nextInt();
  int b=in.nextInt();
+	if(a<b){
  for(int i=a;i<=b;i++){
 	 if(i%2==1){
 		 System.out.println(i+" ");
 	 }
- }
+ }}
+	else{
+		System.out.println("INVALID");
  in.close();
 }
 }

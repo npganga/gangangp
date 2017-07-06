b3a5dbf5063dc66b81e49a3372691332f04f87e7
npganga <gangangpalayam@gmail.com>
2017-07-06 14:39:40 +0530

    Update set15.java

--- set15.java
+++ set15.java
@@ -7,11 +7,15 @@ public class set15 {
 		 Scanner in=new Scanner(System.in);
 		 int a=in.nextInt();
 		 int b=in.nextInt();
+		if(a<b){
 		 for(int i=a;i<=b;i++){
 			 if(i%2==0){
 				 System.out.println(i+" ");
 			 }
-		 }
+		 }}
+		else{
+			System.out.println("INVALID");
+		}
 		 in.close();
 		}
 }

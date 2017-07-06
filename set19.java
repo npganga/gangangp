245fb66d1a2cd56fd5e4e2baab46a0119961e797
npganga <gangangpalayam@gmail.com>
2017-07-06 16:05:25 +0530

    Add files via upload

---
+++ set19.java
@@ -0,0 +1,13 @@
+package beginner;
+import java.util.Scanner;
+public class set19 {
+	public static void main(String[] args){
+		int factorial=1,i,number;
+		Scanner input=new Scanner(System.in);
+		number=input.nextInt();
+		for(i=1;i<=number;i++)
+			factorial*=i;
+	System.out.println("The factorial of the given number "+number+" is "+factorial);
+	input.close();
+	}
+}

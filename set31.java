40075a2573d2e2dfa69eef3d9f7800688b9c7427
npganga <gangangpalayam@gmail.com>
2017-07-19 16:17:33 +0530

    Add files via upload

---
+++ set31.java
@@ -0,0 +1,12 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class set31 {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	String input=in.nextLine();
+	int n=input.length();
+	System.out.println(n);
+}
+}

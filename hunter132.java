b7921d2435aefe81b3b9a14f159b6febeb710b6f
npganga <gangangpalayam@gmail.com>
2017-07-22 14:27:15 +0530

    Add files via upload

---
+++ hunter132.java
@@ -0,0 +1,21 @@
+package Hunter;
+
+import java.util.Scanner;
+
+public class hunter132 {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	for(int i=n;i>0;i--){
+		for(int k=0;k<2;k++){
+		for(int j=i;j>0;j--){
+			System.out.print("*");
+		}
+		System.out.print(" ");
+		}
+		System.out.print("\n");
+	
+	
+	}
+}
+}
